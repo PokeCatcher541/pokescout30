9271566e01f110849e244375d2ad412a9a809654
PokeCatcher541 <wagon.qual.again@cloak.id>
2026-09-21 21:48:12 -0700

    Add product list for Pokemon

--- products.py
+++ products.py
@@ -1,1 +1,31 @@
+PRODUCTS = [
 
+    {
+        "name": "30th Celebration Elite Trainer Box",
+        "store": "Pokémon Center",
+        "category": "30th Celebration",
+        "url": "https://www.pokemoncenter.com/"
+    },
+
+    {
+        "name": "30th Celebration Booster Bundle",
+        "store": "Pokémon Center",
+        "category": "30th Celebration",
+        "url": "https://www.pokemoncenter.com/"
+    },
+
+    {
+        "name": "30th Celebration Mini Tins",
+        "store": "Pokémon Center",
+        "category": "30th Celebration",
+        "url": "https://www.pokemoncenter.com/"
+    },
+
+    {
+        "name": "30th Celebration Tech Sticker Collection",
+        "store": "Pokémon Center",
+        "category": "30th Celebration",
+        "url": "https://www.pokemoncenter.com/"
+    }
+
+]
